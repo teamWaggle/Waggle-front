ba4082aec8622e28dcf9ff229e2927fb86bb21ed
sikkzz <sling0623@gmail.com>
2023-12-11 21:45:46 +0900

    issue #10 design: sort button styling

--- src/components/Landing/Main/Main.tsx
+++ src/components/Landing/Main/Main.tsx
@@ -1,15 +1,23 @@
 import Searchbar from "../Searchbar/Searchbar";
 import Sidebar from "../Sidebar/Sidebar";
+import SortButton from "../SortButton/SortButton";
+
+import Flex from "@components/common/Flex/Flex";
 
 import { mainStyle } from "./Main.styles";
 
 const Main = () => {
 	return (
 		<div css={mainStyle}>
-			<section>
-				<Searchbar />
-			</section>
-			<Sidebar />
+			<Flex>
+				<section>
+					<Searchbar />
+					<Flex styles={{ justify: "flex-end", marginTop: "30px" }}>
+						<SortButton />
+					</Flex>
+				</section>
+				<Sidebar />
+			</Flex>
 		</div>
 	);
 };
---
+++ src/components/Landing/SortButton/SortButton.style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const LayoutDiv = styled.div`
+	padding: 12px 14px;
+	border-radius: 6px;
+	border: 1px solid #d2d2d2;
+	display: flex;
+	align-items: center;
+	gap: 15px;
+
+	& > p {
+		color: #292929;
+		font-family: Pretendard;
+		font-size: 20px;
+		font-weight: 600;
+		line-height: normal;
+	}
+`;
