f35f9dfa98042d1fba733388f8d96a3fe993e80e
sikkzz <sling0623@gmail.com>
2023-12-14 01:09:22 +0900

    issue #18 feat: story book setting

---
+++ .storybook/preview.ts
@@ -0,0 +1,20 @@
+import type { Preview } from "@storybook/react";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+
+const preview: Preview = {
+	parameters: {
+		actions: { argTypesRegex: "^on[A-Z].*" },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+		viewport: {
+			viewports: INITIAL_VIEWPORTS,
+			defaultViewport: "iphone12mini",
+		},
+	},
+};
+
+export default preview;
