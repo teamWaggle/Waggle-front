a33ad4ffad12501bd2074afb219be636eb712716
sikkzz <sling0623@gmail.com>
2023-12-13 21:20:07 +0900

    issue #11 design: siren main styling

---
+++ src/components/Siren/Main/Main.style.ts
@@ -0,0 +1,8 @@
+import { css } from "@emotion/react";
+
+export const sectionStyle = css({
+	maxWidth: "1536px",
+	margin: "0 auto",
+	padding: "0 146px",
+	marginTop: "50px",
+});
---
+++ src/components/Siren/Main/Main.tsx
@@ -0,0 +1,58 @@
+import { sectionStyle } from "./Main.style";
+
+import Flex from "@/components/common/Flex/Flex";
+
+import SirenCard from "../SirenCard/SirenCard";
+
+const mockItems = [
+	{
+		id: 1,
+	},
+	{
+		id: 2,
+	},
+	{
+		id: 3,
+	},
+	{
+		id: 4,
+	},
+	{
+		id: 5,
+	},
+	{
+		id: 6,
+	},
+	{
+		id: 7,
+	},
+	{
+		id: 8,
+	},
+	{
+		id: 9,
+	},
+	{
+		id: 10,
+	},
+];
+
+const Main = () => {
+	return (
+		<section css={sectionStyle}>
+			<Flex
+				styles={{
+					align: "center",
+					wrap: "wrap",
+					gap: "22px",
+				}}
+			>
+				{mockItems.map((data) => (
+					<SirenCard key={data.id} />
+				))}
+			</Flex>
+		</section>
+	);
+};
+
+export default Main;
