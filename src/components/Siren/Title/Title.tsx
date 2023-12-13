4ef36e3294793159847db372b64f8f729b182975
sikkzz <sling0623@gmail.com>
2023-12-13 21:06:37 +0900

    issue #11 design: siren title styling, refactor: search styling refactoring

---
+++ src/components/Siren/Title/Title.tsx
@@ -0,0 +1,63 @@
+import {
+	sectionStyle,
+	tagStyle,
+	searchStyle,
+	inputStyle,
+	buttonStyle,
+} from "./Title.style";
+
+import Flex from "@components/common/Flex/Flex";
+
+import SortButton from "@/components/Landing/SortButton/SortButton";
+
+import SearchButtonIcon from "@assets/svg/search-button.svg?react";
+
+const tagItems = [
+	{
+		title: "전체",
+	},
+	{
+		title: "임시보호",
+	},
+	{
+		title: "강아지 찾아요",
+	},
+	{
+		title: "주인 찾아요",
+	},
+	{
+		title: "기타",
+	},
+];
+
+const Title = () => {
+	return (
+		<section css={sectionStyle}>
+			<Flex styles={{ justify: "space-between", align: "center" }}>
+				<Flex styles={{ gap: "16px" }}>
+					<SortButton />
+					<SortButton />
+				</Flex>
+				<Flex styles={{ gap: "16px" }}>
+					{tagItems.map((data) => (
+						<Flex
+							styles={{ justify: "center", align: "center" }}
+							css={tagStyle}
+							key={data.title}
+						>
+							{data.title}
+						</Flex>
+					))}
+				</Flex>
+				<Flex styles={{ align: "center" }} css={searchStyle}>
+					<input css={inputStyle} />
+					<button css={buttonStyle}>
+						<SearchButtonIcon />
+					</button>
+				</Flex>
+			</Flex>
+		</section>
+	);
+};
+
+export default Title;
--- src/pages/SirenPage.tsx
+++ src/pages/SirenPage.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@components/Landing/Header";
 import Bio from "@components/Siren/Bio/Bio";
+import Title from "@components/Siren/Title/Title";
 
 const SirenPage = () => {
 	return (
 		<>
 			<Header />
 			<Bio />
+			<Title />
 		</>
 	);
 };
