31d6d1441b2838a196207d21f62773b2c7652c2d
sikkzz <sling0623@gmail.com>
2024-01-02 22:17:03 +0900

    issue #32 refactor: siren design refactoring

--- src/components/Siren/Bio/Bio.style.ts
+++ src/components/Siren/Bio/Bio.style.ts
@@ -1,23 +1,24 @@
 import { Theme } from "@/styles/Theme";
 import { css } from "@emotion/react";
 
-export const sectionStyle = css({
+export const boxStyle = css({
 	maxWidth: "1536px",
 	margin: "0 auto",
-	padding: "0 146px",
+	padding: "0 196px",
 });
 
 export const titleStyle = css({
 	color: Theme.color.text,
 	fontFamily: "Pretendard",
-	fontSize: "30px",
 	fontWeight: 700,
-	lineHeight: "normal",
 	letterSpacing: "-0.62px",
 });
 
 export const buttonStyle = css({
 	all: "unset",
 	backgroundColor: Theme.color.brand_primary,
-	padding: "8px",
+	padding: "6px 12px",
+	fontWeight: 600,
+	color: Theme.color.white,
+	borderRadius: "4px",
 });
