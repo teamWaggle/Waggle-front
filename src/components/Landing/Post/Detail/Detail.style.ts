fe8aeb1d3f79f16b2d523db8eb8293dc94cea1f8
sikkzz <sling0623@gmail.com>
2024-01-08 16:25:45 +0900

    issue #38 design: story detail, comment styling

--- src/components/Landing/Post/Detail/Detail.style.ts
+++ src/components/Landing/Post/Detail/Detail.style.ts
@@ -8,4 +8,27 @@ export const layoutStyle = css({
 	borderRadius: "42px",
 	border: `5px solid ${Theme.color.brand_primary}`,
 	boxShadow: Theme.boxShadow.shadow1,
+	position: "relative",
+});
+
+export const imgStyle = css({
+	width: "100%",
+	height: "100%",
+	objectFit: "cover",
+});
+
+export const profileStyle = css({
+	width: "48px",
+	height: "48px",
+	borderRadius: "50%",
+	objectFit: "cover",
+});
+
+export const infoBoxStyle = css({
+	maxWidth: "225px",
+});
+
+export const contentStyle = css({
+	color: Theme.color.input_text,
+	fontWeight: "500",
 });
--- src/components/Landing/Post/Detail/Detail.tsx
+++ src/components/Landing/Post/Detail/Detail.tsx
@@ -1,8 +1,41 @@
-import { Flex } from "@/components/common";
-import { layoutStyle } from "./Detail.style";
+import SampleImg from "@assets/png/post-sample2.png";
+
+import { Flex, Divider, Text } from "@/components/common";
+
+import Comment from "./Comment";
+
+import { layoutStyle, imgStyle, profileStyle, infoBoxStyle, contentStyle } from "./Detail.style";
 
 const Detail = () => {
-	return <Flex css={layoutStyle}>fasdfa</Flex>;
+	return (
+		<Flex css={layoutStyle}>
+			<Flex styles={{ width: "741px", height: "100%", borderRadius: "42px 0 0 42px" }}>
+				<img src={SampleImg} alt="sampleImg" css={imgStyle} />
+			</Flex>
+
+			<Flex styles={{ direction: "column" }}>
+				<Flex styles={{ direction: "column", padding: "36px 16px" }}>
+					<Flex styles={{ gap: "8px" }}>
+						<img src={SampleImg} alt="profileImg" css={profileStyle} />
+						<Flex styles={{ direction: "column" }} css={infoBoxStyle}>
+							<Text size="xSmall" css={contentStyle}>
+								강아지 댕댕댕
+							</Text>
+							<Text size="small" css={contentStyle}>
+								딴은 밤을 세워 우는 벌레는 부끄러운 이 름을 슬퍼하는 까닭입니다. 딴은 밤을 세 워
+								우는 벌레는 부끄러운 이름을 슬퍼하는 까닭입니다.
+							</Text>
+						</Flex>
+					</Flex>
+				</Flex>
+				<Divider length="calc(100% - 5px)" />
+				<Comment />
+				<Comment />
+				<Comment />
+				<Comment />
+			</Flex>
+		</Flex>
+	);
 };
 
 export default Detail;
