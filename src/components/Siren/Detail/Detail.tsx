66013e8f1bd9b8864652f5e5d4bfaa66bceea567
sikkzz <sling0623@gmail.com>
2024-01-13 01:47:53 +0900

    issue #44 feat: create useSirenQuery, siren content api rendering

--- src/components/Siren/Detail/Detail.tsx
+++ src/components/Siren/Detail/Detail.tsx
@@ -1,4 +1,8 @@
+import { useParams } from "react-router-dom";
+
 import { Flex, Box, Divider } from "@components/common";
+
+import { useSirenQuery } from "@/hooks/api/useSirenQuery";
 
 import { layoutStyle } from "./Detail.style";
 
@@ -7,13 +11,38 @@ import Content from "./Content/Content";
 import Comment from "./Comment/Comment";
 
 const Detail = () => {
+	const param = useParams();
+
+	const { siren } = useSirenQuery(Number(param.id));
+	const sirenData = siren && siren.result;
+
 	return (
 		<Box tag="main">
-			<Flex styles={{ direction: "column" }} css={layoutStyle}>
-				<Title />
-				<Divider />
-				<Content />
-			</Flex>
+			{siren && (
+				<Flex styles={{ direction: "column" }} css={layoutStyle}>
+					<Title
+						profileImg={sirenData.profileImg}
+						category={sirenData.category}
+						title={sirenData.title}
+						username={sirenData.username}
+						lostDate={sirenData.lostDate}
+					/>
+					<Divider />
+					<Content
+						lostLocate={sirenData.lostLocate}
+						petKind={sirenData.petKind}
+						petGender={sirenData.petGender}
+						lostDate={sirenData.lostDate}
+						petAge={sirenData.petAge}
+						contact={sirenData.contact}
+						medias={sirenData.medias}
+						content={sirenData.content}
+						recommendIt={sirenData.recommendIt}
+						recommendCount={sirenData.recommendCount}
+					/>
+				</Flex>
+			)}
+
 			<Divider />
 
 			<Comment />
