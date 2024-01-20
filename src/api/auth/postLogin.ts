ca10c5822f822730710f0652a80b67f8de966fda
sikkzz <sling0623@gmail.com>
2024-01-21 02:05:52 +0900

    issue #54 feat: create post sign up, useSignUpMutation

--- src/api/auth/postLogin.ts
+++ src/api/auth/postLogin.ts
@@ -1,11 +1,12 @@
 import type { AxiosResponse } from "axios";
 
 import { axiosInstance } from "@/api/axiosInstance";
+import { END_POINTS } from "@/constants/api";
 
 import type { TokenType, UserType } from "@/types/auth";
 
 export const postLogIn = async ({ username, password }: UserType) => {
-	const { data } = await axiosInstance.post<UserType, AxiosResponse<TokenType>>("/api/tokens", {
+	const { data } = await axiosInstance.post<UserType, AxiosResponse<TokenType>>(END_POINTS.LOGIN, {
 		username,
 		password,
 	});
---
+++ src/api/auth/postSignUp.ts
@@ -0,0 +1,26 @@
+import { axiosInstance } from "@/api/axiosInstance";
+import { END_POINTS } from "@/constants/api";
+
+import type { FormDataType } from "@/types/auth";
+
+export const postSignUp = async (formData: FormData) => {
+	const config = {
+		headers: { "Content-Type": "multipart/form-data" },
+	};
+
+	// const { data } = await axiosInstance.post<SignUpFormType, AxiosResponse<SignUpResponseType>>(
+	// 	END_POINTS.SIGN_UP,
+	// 	formData,
+	// 	config,
+	// );
+
+	return axiosInstance.post<FormDataType>(
+		END_POINTS.SIGN_UP,
+		{
+			formData,
+		},
+		config,
+	);
+
+	// return data;
+};
