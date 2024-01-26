2944b5b803864d3f4ffc06f86fd1d40472988fee
sikkzz <sling0623@gmail.com>
2024-01-26 23:37:45 +0900

    issue #54 feat: create email auth send

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,6 +1,7 @@
 export const END_POINTS = {
 	LOGIN: "/api/tokens",
 	SIGN_UP: "/api/members",
+	EMAIL_AUTH_SEND: "/api/members/email/send",
 	STORY: (boardId: number) => `/api/stories/${boardId}`,
 	STORY_LIST: (currentPage: number) => `/api/stories?currentPage=${currentPage}`,
 	COMMENT: (currentPage: number, boardId: number) =>
