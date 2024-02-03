3e54b876f5f57ff98f4fcb4f68e94f5cb73723ed
sikkzz <sling0623@gmail.com>
2024-02-03 19:28:55 +0900

    issue #54 feat: create password validator

---
+++ src/utils/validator.ts
@@ -0,0 +1,17 @@
+import { REGEX } from "@/constants/regex";
+
+export const hasEngInPassword = (password: string) => {
+	return REGEX.ENG_REG.test(password);
+};
+
+export const hasNumInPassword = (password: string) => {
+	return REGEX.NUM_REG.test(password);
+};
+
+export const hasSpecialCharInPassword = (password: string) => {
+	return REGEX.SPECIAL_CHAR_REG.test(password);
+};
+
+export const checkLengthInPassword = (password: string) => {
+	return password.length > 8 && password.length < 20;
+};
