199a703040e0b1f433ed2e020aab362a23a3fb77
sikkzz <sling0623@gmail.com>
2024-02-14 01:40:45 +0900

    issue #63 refactor: env add, login param change, logout api change

--- src/api/auth/deleteLogout.ts
+++ src/api/auth/deleteLogout.ts
@@ -1,5 +1,6 @@
 import { axiosInstance } from "@/api/axiosInstance";
+import { END_POINTS } from "@/constants/api";
 
 export const deleteLogout = () => {
-	return axiosInstance.delete("/api/tokens");
+	return axiosInstance.delete(END_POINTS.LOGIN);
 };
