ba01e5aee956c7b7c5a442dfc9c46e36e1fd81f7
sikkzz <sling0623@gmail.com>
2024-02-15 03:11:38 +0900

    issue #63 refactor: axiosInstance change to authorizedAxiosInstance

--- src/api/auth/postEmailAuthSend.ts
+++ src/api/auth/postEmailAuthSend.ts
@@ -1,10 +1,13 @@
 import { AxiosResponse } from "axios";
 
-import { axiosInstance } from "@/api/axiosInstance";
+import { authorizedAxiosInstance } from "@/api/axiosInstance";
 import { END_POINTS } from "@/constants/api";
 
 import type { SignUpResponseType } from "@/types/auth";
 
 export const postEmailAuthSend = (email: string) => {
-	return axiosInstance.post<AxiosResponse<SignUpResponseType>>(END_POINTS.EMAIL_AUTH_SEND, email);
+	return authorizedAxiosInstance.post<AxiosResponse<SignUpResponseType>>(
+		END_POINTS.EMAIL_AUTH_SEND,
+		email,
+	);
 };
