dac19e7646a23af0e262e48e7b890a8343f7f9a3
sikkzz <sling0623@gmail.com>
2024-02-16 19:41:27 +0900

    issue #65 refactor: sign up tab refactoring

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 
 import { css } from "@emotion/react";
 
@@ -6,24 +7,32 @@ import { Flex } from "@/components/common";
 import SignUpEmail from "@/components/SignUp/Email/SignUpEmail";
 import SignUpPet from "@/components/SignUp/Pet/SignUpPet";
 import SignUpProfile from "@/components/SignUp/Profile/SignUpProfile";
-import SignUpTitle from "@/components/SignUp/SignUpTitle";
+import SignUpTab from "@/components/SignUp/SignUpTab";
+import { SIGN_UP_TAB_KEY, TAB_KEY } from "@/constants/tab";
 
 const SignUp = () => {
-	const [titleTab, setTitleTab] = useState("이메일 인증");
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	useEffect(() => {
+		if (
+			!searchParams ||
+			(searchParams.get(TAB_KEY) !== SIGN_UP_TAB_KEY.EMAIL &&
+				searchParams.get(TAB_KEY) !== SIGN_UP_TAB_KEY.PROFILE &&
+				searchParams.get(TAB_KEY) !== SIGN_UP_TAB_KEY.PET)
+		) {
+			setSearchParams(`${TAB_KEY}=${SIGN_UP_TAB_KEY.EMAIL}`);
+		}
+	}, []);
 
 	return (
 		<Flex styles={{ direction: "column", align: "center" }} css={layoutStyle}>
-			<SignUpTitle tab={titleTab} changeTab={setTitleTab} />
+			<SignUpTab />
 
-			{titleTab === "이메일 인증" ? (
-				<SignUpEmail />
-			) : titleTab === "프로필 입력" ? (
-				<SignUpProfile />
-			) : titleTab === "반려견 등록" ? (
-				<SignUpPet />
-			) : (
-				""
-			)}
+			{searchParams.get(TAB_KEY) === SIGN_UP_TAB_KEY.EMAIL && <SignUpEmail />}
+
+			{searchParams.get(TAB_KEY) === SIGN_UP_TAB_KEY.PROFILE && <SignUpProfile />}
+
+			{searchParams.get(TAB_KEY) === SIGN_UP_TAB_KEY.PET && <SignUpPet />}
 		</Flex>
 	);
 };
