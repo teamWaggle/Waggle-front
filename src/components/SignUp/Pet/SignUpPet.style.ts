1d823350d2160ddece88b495873f423260aaa9c6
sikkzz <sling0623@gmail.com>
2024-02-17 17:02:16 +0900

    issue #65 refactor: sign up pet layout style change

--- src/components/SignUp/Pet/SignUpPet.style.ts
+++ src/components/SignUp/Pet/SignUpPet.style.ts
@@ -1,8 +1,18 @@
 import { css } from "@emotion/react";
 
+import { Theme } from "@/styles/Theme";
+
 export const imgStyle = css({
 	width: "148px",
 	height: "148px",
 	borderRadius: "10px",
 	objectFit: "cover",
+});
+
+export const formLayoutStyle = css({
+	flexDirection: "column",
+	gap: "30px",
+	borderRadius: "2px",
+	border: `1px solid ${Theme.color.border}`,
+	padding: "60px",
 });
