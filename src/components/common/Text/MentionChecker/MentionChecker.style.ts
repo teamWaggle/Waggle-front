a1ef79349827499f4f642ad86e1a261fc715949b
DongHyeonWon <dhe7700@naver.com>
2024-02-18 04:58:44 +0900

    issue #64 feat: Create Comment

--- src/components/common/Text/MentionChecker/MentionChecker.style.ts
+++ src/components/common/Text/MentionChecker/MentionChecker.style.ts
@@ -2,19 +2,23 @@ import { css } from "@emotion/react";
 
 import { Theme } from "@/styles/Theme";
 
+export const mentionBoxStyle = css({
+	wordBreak: "break-word",
+	width: "300px",
+	display: "inline",
+	whiteSpace: "normal",
+
+	// width: "fit-content",	fontSize: "12px",
+});
+
 export const mentionCheckerStyle = css({
 	color: Theme.color.brand_primary,
 	display: "inline",
+	fontSize: "14px",
 });
 
 export const mentionCheckerDefaultStyle = css({
 	color: Theme.color.text,
 	display: "inline",
-});
-
-export const mentionBoxStyle = css({
-	wordBreak: "break-word",
-	width: "350px",
-	whiteSpace: "normal",
-	// width: "fit-content",
+	fontSize: "14px",
 });
