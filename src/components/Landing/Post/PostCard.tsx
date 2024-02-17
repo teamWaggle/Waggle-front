d5f3765f5aed1a576b554cfc2c2616a72adec581
sikkzz <sling0623@gmail.com>
2024-02-17 17:38:22 +0900

    issue #61 feat: create story upload button, media many icon

--- src/components/Landing/Post/PostCard.tsx
+++ src/components/Landing/Post/PostCard.tsx
@@ -1,15 +1,17 @@
+import MediaIcon from "@/assets/svg/ic-many-media.svg?react";
+
 import { Flex } from "@/components/common";
 import Detail from "@/components/Landing/Post/Detail/Detail";
 import { StoryListInfoType } from "@/types/story";
 
 import useModal from "@/hooks/useModal";
 
-import { imgStyle } from "@/components/Landing/Post/Post.style";
+import { imgStyle, iconStyle } from "@/components/Landing/Post/Post.style";
 
 const PostCard = ({ id, thumbnail }: StoryListInfoType) => {
 	const modal = useModal();
 
-	const open = () => {
+	const postDetailOpen = () => {
 		modal.openModal({
 			key: `postDetail${id}`,
 			component: () => <Detail id={id} />,
@@ -17,8 +19,12 @@ const PostCard = ({ id, thumbnail }: StoryListInfoType) => {
 	};
 
 	return (
-		<Flex styles={{ align: "center", width: "252px", height: "252px" }} onClick={open}>
+		<Flex
+			styles={{ align: "center", width: "252px", height: "252px", position: "relative" }}
+			onClick={postDetailOpen}
+		>
 			<img src={thumbnail} alt="profileImg" css={imgStyle} />
+			<MediaIcon css={iconStyle} />
 		</Flex>
 	);
 };
