131be3b064c3215ebfcbc4a12d7c2bbc86b44156
sikkzz <sling0623@gmail.com>
2024-02-17 17:43:13 +0900

    issue #61 refactor: story detail content, comment refactoring

--- src/components/Landing/Post/Detail/Detail.tsx
+++ src/components/Landing/Post/Detail/Detail.tsx
@@ -1,17 +1,19 @@
 import SampleImg from "@/assets/png/post-sample2.png";
 
-import { Flex, Divider, Text } from "@/components/common";
+import { Flex, Box, Divider, Text } from "@/components/common";
 import Comment from "@/components/Landing/Post/Detail/Comment";
 
 import { useCommentQuery } from "@/hooks/api/useCommentQuery";
 import { useStoryQuery } from "@/hooks/api/useStoryQuery";
 
+import { getDefaultTextStyle } from "@/styles/getDefaultTextStyle";
+import { Theme } from "@/styles/Theme";
+
 import {
 	layoutStyle,
 	imgStyle,
 	profileStyle,
-	infoBoxStyle,
-	contentStyle,
+	contentBoxStyle,
 } from "@/components/Landing/Post/Detail/Detail.style";
 
 interface idType {
@@ -25,29 +27,61 @@ const Detail = ({ id }: idType) => {
 
 	const imgIndex = 0;
 
+	console.log(commentData);
+
 	return (
 		<>
 			{storyData && (
 				<Flex css={layoutStyle}>
-					<Flex styles={{ width: "741px", height: "100%", borderRadius: "42px 0 0 42px" }}>
+					{/* 미디어 영역 */}
+					<Flex
+						styles={{
+							width: "741px",
+							height: "100%",
+							borderRadius: "42px 0 0 42px",
+							borderRight: "1px solid #d2d2d2",
+						}}
+					>
 						<img src={storyData.result.medias[imgIndex]} alt="profileImg" css={imgStyle} />
 					</Flex>
 
+					{/* 본문 영역 */}
 					<Flex styles={{ direction: "column" }}>
-						<Flex styles={{ direction: "column", padding: "36px 16px" }}>
-							<Flex styles={{ gap: "8px" }}>
+						<Flex
+							styles={{
+								direction: "column",
+								padding: "52px 30px 12px 18px",
+								gap: "12px",
+								width: "100%",
+							}}
+						>
+							{/* 프로필 영역 */}
+							<Flex styles={{ gap: "10px", align: "center" }}>
 								<img src={SampleImg} alt="profileImg" css={profileStyle} />
-								<Flex styles={{ direction: "column" }} css={infoBoxStyle}>
-									<Text size="xSmall" css={contentStyle}>
-										{storyData.result.username}
-									</Text>
-									<Text size="small" css={contentStyle}>
-										{storyData.result.content}
-									</Text>
-								</Flex>
+								<Text size="small" css={getDefaultTextStyle(Theme.color.input_text, 700)}>
+									{storyData.result.username}
+								</Text>
+							</Flex>
+
+							{/* 콘텐츠 본문 영역 */}
+							<Box css={contentBoxStyle}>
+								<Text css={getDefaultTextStyle(Theme.color.input_text, 500)}>
+									{storyData.result.content}
+								</Text>
+							</Box>
+
+							{/* 게시 날짜 영역 */}
+							<Flex styles={{ justify: "flex-end", width: "100%" }}>
+								<Text size="xSmall" css={getDefaultTextStyle(Theme.color.readonly_text, 500)}>
+									{storyData.result.createdDate}
+								</Text>
 							</Flex>
 						</Flex>
+
+						{/* 구분선 */}
 						<Divider length="309px" />
+
+						{/* 코멘트 영역 */}
 						{commentData &&
 							commentData.result.commentList.map((comment) => (
 								<Comment key={comment.id} {...comment} />
