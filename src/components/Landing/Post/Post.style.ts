40d7dc7f8c484449a14ba38ae6d43a1c52a6122f
sikkzz <sling0623@gmail.com>
2024-02-17 17:38:22 +0900

    issue #61 feat: create story upload button, media many icon

--- src/components/Landing/Post/Post.style.ts
+++ src/components/Landing/Post/Post.style.ts
@@ -9,6 +9,12 @@ export const imgStyle = css({
 	borderRadius: "4px",
 });
 
+export const iconStyle = css({
+	position: "absolute",
+	top: "12px",
+	right: "12px",
+});
+
 export const headingStyle = css({
 	color: Theme.color.brand_primary,
 	fontFamily: "Montserrat",
@@ -17,5 +23,18 @@ export const headingStyle = css({
 
 	"& > svg": {
 		marginLeft: "4px",
+	},
+});
+
+export const buttonStyle = css({
+	all: "unset",
+	padding: "6px 8px",
+	backgroundColor: Theme.color.brand_primary,
+	color: Theme.color.white,
+	borderRadius: "4px",
+
+	"& > p": {
+		lineHeight: "24px",
+		fontWeight: 600,
 	},
 });
--- src/components/Landing/Post/PostTitle.tsx
+++ src/components/Landing/Post/PostTitle.tsx
@@ -1,17 +1,23 @@
 import LogIcon from "@/assets/svg/log.svg?react";
 
-import { Flex, Heading } from "@/components/common";
+import { Flex, Heading, Text } from "@/components/common";
 import { SortButton } from "@/components/Landing";
 
-import { headingStyle } from "@/components/Landing/Post/Post.style";
+import { headingStyle, buttonStyle } from "@/components/Landing/Post/Post.style";
 
 const PostTitle = () => {
 	return (
 		<Flex styles={{ align: "center", justify: "space-between", width: "100%" }}>
-			<Heading size="large" css={headingStyle}>
-				Waggle LOG
-				<LogIcon />
-			</Heading>
+			<Flex styles={{ align: "center", gap: "20px" }}>
+				<Heading size="large" css={headingStyle}>
+					Waggle LOG
+					<LogIcon />
+				</Heading>
+				<button type="button" css={buttonStyle}>
+					<Text size="xLarge">글 작성하기</Text>
+				</button>
+			</Flex>
+
 			<SortButton defaultText="인기순" />
 		</Flex>
 	);
