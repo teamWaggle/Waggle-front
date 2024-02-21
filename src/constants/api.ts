2a0ade5bcedaa0ca4b1bd82e3c645cc684b3ca30
sikkzz <sling0623@gmail.com>
2024-02-21 22:15:35 +0900

    issue #57 refactor: api url refactoring

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -8,7 +8,7 @@ export const END_POINTS = {
 	EMAIL_AUTH_VERIFY: "/api/members/email/verify",
 	MEMBER_INFO_FIRST: "/api/members/info",
 	FIND_EMAIL: (name: string, birthday: string) =>
-		`/api/member/email/find?name=${name}&birthday=${birthday}`,
+		`/api/members/email/find?name=${name}&birthday=${birthday}`,
 	CHECK_NICKNAME: (nickname: string) => `/api/members/check-nickname?nickname=${nickname}`,
 	STORY: (boardId: number) => `/api/stories/${boardId}`,
 	STORY_LIST: (currentPage: number) => `/api/stories?currentPage=${currentPage}`,
