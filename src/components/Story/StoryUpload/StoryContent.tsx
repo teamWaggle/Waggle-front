366fdafb8fdb97e73708506ea66899c91f40fdbd
sikkzz <sling0623@gmail.com>
2024-02-28 17:35:29 +0900

    issue #100 refactor: upload button change

--- src/components/Story/StoryUpload/StoryContent.tsx
+++ src/components/Story/StoryUpload/StoryContent.tsx
@@ -12,9 +12,11 @@ import {
 	layoutStyle,
 	headerStyle,
 	imgBoxStyle,
+	contentBoxStyle,
 	profileImgStyle,
 	textareaStyle,
 	lengthTextStyle,
+	uploadButtonStyle,
 } from "@/components/Story/StoryUpload/StoryContent.style";
 
 const StoryContent = ({ media }: { media: string }) => {
@@ -24,40 +26,43 @@ const StoryContent = ({ media }: { media: string }) => {
 		<Flex css={layoutStyle}>
 			<Flex css={headerStyle}>
 				<LeftArrow />
-				<Text size="large" css={getDefaultTextStyle(Theme.color.text, 600)}>
+				<Text size="xLarge" css={getDefaultTextStyle(Theme.color.text, 600)}>
 					글 쓰기
-				</Text>
-				<Text size="xLarge" css={getDefaultTextStyle(Theme.color.brand_primary, 600)}>
-					업로드
 				</Text>
 			</Flex>
 
-			<Flex styles={{ height: "calc(100% - 44px)" }}>
+			<Flex styles={{ height: "calc(100% - 54px)" }}>
 				<Box css={imgBoxStyle}>
 					<img src={media} alt="media" />
 				</Box>
 
-				<Flex styles={{ direction: "column", padding: "14px 28px", gap: "12px", width: "319px" }}>
-					{/* 프로필 */}
-					<Flex styles={{ align: "center", gap: "10px" }}>
-						<img src={SampleImg} alt="profileImg" css={profileImgStyle} />
-						<Text size="small" css={getDefaultTextStyle(Theme.color.text, 700)}>
-							강아지댕댕댕
+				<Flex css={contentBoxStyle}>
+					<Flex styles={{ direction: "column", gap: "12px", width: "100%" }}>
+						{/* 프로필 */}
+						<Flex styles={{ align: "center", gap: "10px" }}>
+							<img src={SampleImg} alt="profileImg" css={profileImgStyle} />
+							<Text size="small" css={getDefaultTextStyle(Theme.color.text, 700)}>
+								강아지댕댕댕
+							</Text>
+						</Flex>
+
+						{/* 본문 입력 */}
+						<textarea
+							css={textareaStyle}
+							placeholder="사진에 대한 설명을 입력해주세요"
+							maxLength={500}
+							value={content}
+							onChange={(e) => setContent(e.target.value)}
+						/>
+
+						{/* 글자수 */}
+						<Text size="small" css={lengthTextStyle}>
+							{content.length}/500
 						</Text>
 					</Flex>
 
-					{/* 본문 입력 */}
-					<textarea
-						css={textareaStyle}
-						placeholder="사진에 대한 설명을 입력해주세요"
-						maxLength={500}
-						value={content}
-						onChange={(e) => setContent(e.target.value)}
-					/>
-
-					{/* 글자수 */}
-					<Text size="small" css={lengthTextStyle}>
-						{content.length}/500
+					<Text size="xLarge" css={uploadButtonStyle}>
+						업로드
 					</Text>
 				</Flex>
 			</Flex>
