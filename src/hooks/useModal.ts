817fd9759de2b50cbf4b17a38123b2f5a92f2fb4
sikkzz <sling0623@gmail.com>
2024-03-03 23:11:19 +0900

    issue #100 refactor: modal icon style refactoring

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,3 +1,5 @@
+import { useCallback } from "react";
+
 import { useSetRecoilState } from "recoil";
 
 import { modalState, scheduleModalState } from "@/recoil/atoms/modal";
@@ -8,19 +10,24 @@ const useModal = () => {
 	const setModals = useSetRecoilState(modalState);
 	const setScheduleModals = useSetRecoilState(scheduleModalState);
 
-	const openModal = ({ key, component }: ModalType) => {
-		const modalProps = {
-			key,
-			close: () => setModals([]),
-			component,
-		};
+	const openModal = useCallback(
+		({ key, component, notCloseIcon, isWhiteIcon }: ModalType) => {
+			const modalProps = {
+				key,
+				close: () => setModals([]),
+				component,
+				notCloseIcon,
+				isWhiteIcon,
+			};
 
-		setModals((prev) => [...prev, modalProps]);
-	};
+			setModals((prev) => [...prev, modalProps]);
+		},
+		[setModals],
+	);
 
-	const closeModal = () => {
+	const closeModal = useCallback(() => {
 		setModals([]);
-	};
+	}, [setModals]);
 
 	const openScheduleModal = ({ key, component }: ModalType) => {
 		const modalProps = {
