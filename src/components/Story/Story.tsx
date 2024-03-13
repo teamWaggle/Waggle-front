ef0ddfc8a6e3d748b45adae0a1d0730282b1d000
sikkzz <sling0623@gmail.com>
2024-03-13 20:41:35 +0900

    issue #117 feat: create infiniti query test api

--- src/components/Story/Story.tsx
+++ src/components/Story/Story.tsx
@@ -1,10 +1,25 @@
+// import { useState, useEffect } from "react";
+
 import { Flex } from "@/components/common";
 import { StoryTitle, StoryCard } from "@/components/Story";
 
 import { useStoryListQuery } from "@/hooks/api/useStoryListQuery";
+// import { useStoryListTestQuery } from "@/hooks/api/useStoryListQuery";
 
 const Story = () => {
+	// const [currentPage, setCurrentPage] = useState(0);
 	const { storyListData } = useStoryListQuery(0);
+	// const { storyListData, hasNextPage, fetchNextPage } = useStoryListTestQuery(currentPage);
+
+	// console.log(storyListData?.pages);
+	// console.log(hasNextPage);
+
+	// useEffect(() => {
+	// 	if (hasNextPage) {
+	// 		setCurrentPage((prev) => prev + 1);
+	// 	}
+	// 	fetchNextPage();
+	// }, [hasNextPage]);
 
 	return (
 		<Flex styles={{ gap: "20px", direction: "column", width: "100%" }}>
--- src/hooks/api/useStoryListQuery.ts
+++ src/hooks/api/useStoryListQuery.ts
@@ -1,6 +1,7 @@
 import type { AxiosError } from "axios";
 
 import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 import { getStoryList } from "@/api/story/getStoryList";
 
@@ -13,4 +14,22 @@ export const useStoryListQuery = (currentPage: number) => {
 	});
 
 	return { storyListData };
+};
+
+export const useStoryListTestQuery = (currentPage: number) => {
+	const {
+		data: storyListData,
+		fetchNextPage,
+		hasNextPage,
+	} = useInfiniteQuery<StoryListType>({
+		queryKey: ["storyList"],
+		queryFn: () => getStoryList(currentPage),
+		initialPageParam: 0,
+		// 결과값 안에 다음 페이지 호출 필드가 필요함
+		getNextPageParam: (currentPage) => {
+			return currentPage.result.isLast;
+		},
+	});
+
+	return { storyListData, fetchNextPage, hasNextPage };
 };
