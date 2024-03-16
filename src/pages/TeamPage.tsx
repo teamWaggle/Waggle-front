7030d43067bd45da0b976ef57aca7666092014cb
DongHyeonWon <dhe7700@naver.com>
2024-03-16 18:49:29 +0900

    issue #99 fix: delete header in teampage

--- src/pages/TeamPage.tsx
+++ src/pages/TeamPage.tsx
@@ -1,11 +1,9 @@
-import Header from "@/components/Header/Header";
 import { TeamInfo } from "@/components/Team";
 import CalendarSection from "@/components/Team/CalendarSection/CalendarSection";
 
 const TeamPage = () => {
 	return (
 		<>
-			<Header />
 			<CalendarSection />
 			<TeamInfo />
 		</>
