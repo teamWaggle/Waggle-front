d1f1e0b32f68a39e8ab352297cf7294429ba8432
sikkzz <sling0623@gmail.com>
2024-03-17 20:24:07 +0900

    issue #112 refactor: siren edit page add

--- src/pages/SirenDetailPage.tsx
+++ src/pages/SirenDetailPage.tsx
@@ -1,9 +1,35 @@
+import { useSearchParams, useParams } from "react-router-dom";
+
 import { Detail } from "@/components/Siren/Detail";
+import SirenEdit from "@/components/Siren/SirenEdit/SirenEdit";
+
+import { useSirenQuery } from "@/hooks/api/useSirenQuery";
 
 const SirenDetailPage = () => {
+	const param = useParams();
+
+	const { sirenData } = useSirenQuery(Number(param.id));
+
+	const [searchParams] = useSearchParams();
+
 	return (
 		<>
-			<Detail />
+			{sirenData && searchParams.get("mode") === "edit" ? (
+				<SirenEdit
+					title={sirenData.result.title}
+					category={sirenData.result.category}
+					lostLocate={sirenData.result.lostLocate}
+					lostDate={sirenData.result.lostDate}
+					petAge={sirenData.result.petAge}
+					petBreed={sirenData.result.petBreed}
+					petGender={sirenData.result.petGender}
+					contact={sirenData.result.contact}
+					content={sirenData.result.content}
+					mediaList={sirenData.result.mediaList}
+				/>
+			) : (
+				<Detail />
+			)}
 		</>
 	);
 };
