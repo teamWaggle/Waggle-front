dd847a6b408a7d2411809b1a8abb4d3ce8a02435
sikkzz <sling0623@gmail.com>
2024-03-18 16:59:58 +0900

    issue #125 feat: create post recommend api

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -32,6 +32,7 @@ export const END_POINTS = {
 	REPLY: (replyId: number) => `/api/replies/${replyId}`,
 	PET: "/api/pets",
 	MEDIA: "/api/media/list",
+	RECOMMEND: (boardId: number) => `/api/recommends/${boardId}`,
 };
 
 export const HTTP_STATUS_CODE = {
