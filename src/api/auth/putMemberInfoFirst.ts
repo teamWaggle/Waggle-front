8268aa7b8106c1e74589ec9b3f4b7e00f0c8197f
sikkzz <sling0623@gmail.com>
2024-03-23 23:33:50 +0900

    issue #134 refactor: sign up profile img upload refactoring

--- src/api/auth/putMemberInfoFirst.ts
+++ src/api/auth/putMemberInfoFirst.ts
@@ -1,23 +1,19 @@
+import type { AxiosResponse } from "axios";
+
 import { authorizedAxiosInstance } from "@/api/axiosInstance";
 
 import { END_POINTS } from "@/constants/api";
 
-export interface FormDataType {
-	memberProfileRequest: SignUpFormType;
-}
-
-export interface SignUpFormType {
-	nickname: string;
-	name: string;
-	birthday: string;
-	userUrl: string;
-	memberProfileImg: string;
-}
+import type { CommonResponseType } from "@/types/common";
 
 export const putMemberInfoFirst = async (formData: FormData) => {
 	const config = {
 		headers: { "Content-Type": "multipart/form-data" },
 	};
 
-	return authorizedAxiosInstance.put<FormDataType>(END_POINTS.MEMBER_INFO_FIRST, formData, config);
+	return await authorizedAxiosInstance.put<FormData, AxiosResponse<CommonResponseType>>(
+		END_POINTS.MEMBER_INFO_FIRST,
+		formData,
+		config,
+	);
 };
--- src/components/SignUp/Profile/ProfileForm/Profile.tsx
+++ src/components/SignUp/Profile/ProfileForm/Profile.tsx
@@ -1,16 +1,9 @@
-import { useState } from "react";
-import { toast } from "react-toastify";
-
 import DefaultProfileImg from "@/assets/png/profile.png";
 
 import { Flex, Text } from "@/components/common";
 
-import { ALLOW_FILE_EXTENSION, FILE_SIZE_MAX_LIMIT } from "@/constants/file";
-
 import { getDefaultTextStyle } from "@/styles/getDefaultTextStyle";
 import { Theme } from "@/styles/Theme";
-
-import { fileExtensionValid } from "@/utils/file";
 
 import { imgStyle } from "@/components/SignUp/Profile/SignUpProfile.style";
 import {
@@ -20,62 +13,30 @@ import {
 } from "@/components/SignUp/SignUp.shared.style";
 
 const Profile = ({
-	// fileURL,
-	changeFile,
+	handleImgUpload,
+	uploadMediaList,
 }: {
-	fileURL: File | undefined;
-	changeFile: React.Dispatch<React.SetStateAction<File | undefined>>;
+	handleImgUpload: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	uploadMediaList: string[];
 }) => {
-	const [, setDefaultImg] = useState<string | ArrayBuffer | null>("");
-
-	const handleChangeImg = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const target = e.currentTarget;
-		const files = (target.files as FileList)[0];
-
-		if (files === undefined) {
-			return;
-		}
-
-		if (!fileExtensionValid(files)) {
-			target.value = "";
-
-			toast.error(`업로드 가능한 확장자가 아닙니다. [가능한 확장자 : ${ALLOW_FILE_EXTENSION}]`);
-
-			return;
-		}
-
-		if (files.size > FILE_SIZE_MAX_LIMIT) {
-			target.value = "";
-			toast.error("업로드 가능한 최대 용량은 1MB입니다. ");
-			return;
-		}
-
-		const reader = new FileReader();
-
-		reader.readAsDataURL(files);
-
-		reader.onloadend = () => {
-			// console.log(reader.result);
-			setDefaultImg(reader.result);
-			changeFile(files);
-		};
-
-		// const newFileURL = URL.createObjectURL(files);
-		// changeFile(newFileURL);
-	};
-
 	return (
 		<Flex styles={{ align: "center", gap: "60px" }}>
-			{/* <img src={defaultImg ? defaultImg : DefaultProfileImg} alt="profileImg" css={imgStyle} /> */}
-			<img src={DefaultProfileImg} alt="profileImg" css={imgStyle} />
+			<img
+				src={uploadMediaList.length !== 0 ? uploadMediaList[0] : DefaultProfileImg}
+				alt="profileImg"
+				css={imgStyle}
+			/>
+
 			<Flex styles={{ direction: "column", gap: "14px" }}>
 				<Text css={getFormTextStyle(false)}>프로필 이미지</Text>
-				<input type="file" id="profileImg" onChange={handleChangeImg} css={inputNoneDisplayStyle} />
+
+				<input type="file" id="profileImg" onChange={handleImgUpload} css={inputNoneDisplayStyle} />
 				<label htmlFor="profileImg" css={commonButtonStyle}>
 					<Text css={getDefaultTextStyle(Theme.color.disabled_text, 500)}>
 						컴퓨터에서 파일 선택
 					</Text>
 				</label>
+
 				<Text size="small" css={getDefaultTextStyle(Theme.color.disabled_text, 500)}>
 					확장자: png, jpg, jpeg / 용량: 1MB 이하
 				</Text>
