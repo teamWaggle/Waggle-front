519f43c5898227ad6002e15dc3514686045376e7
sikkzz <sling0623@gmail.com>
2024-03-23 23:47:44 +0900

    issue #134 refactor: sign up pet profile img upload refactoring

--- src/components/SignUp/Pet/PetForm/PetProfile.tsx
+++ src/components/SignUp/Pet/PetForm/PetProfile.tsx
@@ -1,15 +1,9 @@
-import { toast } from "react-toastify";
-
 import DefaultProfileImg from "@/assets/png/pet-profile.png";
 
 import { Flex, Text } from "@/components/common";
 
-import { ALLOW_FILE_EXTENSION, FILE_SIZE_MAX_LIMIT } from "@/constants/file";
-
 import { getDefaultTextStyle } from "@/styles/getDefaultTextStyle";
 import { Theme } from "@/styles/Theme";
-
-import { fileExtensionValid } from "@/utils/file";
 
 import { imgStyle } from "@/components/SignUp/Pet/SignUpPet.style";
 import {
@@ -19,45 +13,22 @@ import {
 } from "@/components/SignUp/SignUp.shared.style";
 
 const PetProfile = ({
-	fileURL,
-	changeFile,
+	handleImgUpload,
+	uploadMediaList,
 }: {
-	fileURL: string;
-	changeFile: React.Dispatch<React.SetStateAction<string>>;
+	handleImgUpload: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	uploadMediaList: string[];
 }) => {
-	const handleChangeImg = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const target = e.currentTarget;
-		const files = (target.files as FileList)[0];
-
-		if (files === undefined) {
-			return;
-		}
-
-		if (!fileExtensionValid(files)) {
-			target.value = "";
-
-			toast.error(`업로드 가능한 확장자가 아닙니다. [가능한 확장자 : ${ALLOW_FILE_EXTENSION}]`);
-
-			return;
-		}
-
-		if (files.size > FILE_SIZE_MAX_LIMIT) {
-			target.value = "";
-			toast.error("업로드 가능한 최대 용량은 1MB입니다. ");
-			return;
-		}
-
-		const newFileURL = URL.createObjectURL(files);
-
-		changeFile(newFileURL);
-	};
-
 	return (
 		<Flex styles={{ align: "center", gap: "60px" }}>
-			<img src={fileURL ? fileURL : DefaultProfileImg} alt="petProfileImg" css={imgStyle} />
+			<img
+				src={uploadMediaList.length !== 0 ? uploadMediaList[0] : DefaultProfileImg}
+				alt="petProfileImg"
+				css={imgStyle}
+			/>
 			<Flex styles={{ direction: "column", gap: "14px" }}>
 				<Text css={getFormTextStyle(false)}>프로필 이미지</Text>
-				<input type="file" id="profileImg" onChange={handleChangeImg} css={inputNoneDisplayStyle} />
+				<input type="file" id="profileImg" onChange={handleImgUpload} css={inputNoneDisplayStyle} />
 				<label htmlFor="profileImg" css={commonButtonStyle}>
 					<Text css={getDefaultTextStyle(Theme.color.disabled_text, 500)}>
 						컴퓨터에서 파일 선택
