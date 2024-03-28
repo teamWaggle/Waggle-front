1f932cc3204d43db08de5e6fa1314d2e18f73792
sikkzz <sling0623@gmail.com>
2024-03-28 19:49:48 +0900

    issue #148 feat: create post question mutation

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -35,6 +35,7 @@ export const END_POINTS = {
 	QUESTIONS: (currentPage: number) => `/api/questions?currentPage=${currentPage}`,
 	QUESTION: (questionId: number) => `/api/questions/${questionId}`,
 	QUESTION_REPRESENTATIVE: `/api/questions/representative`,
+	POST_QUESTION: "/api/questions",
 };
 
 export const HTTP_STATUS_CODE = {
