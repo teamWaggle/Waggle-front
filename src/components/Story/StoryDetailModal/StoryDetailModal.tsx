a9c77e45d68a01509e9eab6e8280be184e1823b2
sikkzz <sling0623@gmail.com>
2024-04-01 03:48:44 +0900

    issue #165 refactor: story detail modal props refactoring

--- src/components/Story/StoryDetailModal/StoryDetailModal.tsx
+++ src/components/Story/StoryDetailModal/StoryDetailModal.tsx
@@ -12,13 +12,14 @@ import StoryUploadModal from "@/components/Story/StoryUploadModal/StoryUploadMod
 import { useCommentQuery } from "@/hooks/api/comment/useCommentQuery";
 import { useEditCommentMutation } from "@/hooks/api/comment/useEditCommentMutation";
 import { usePostCommentMutation } from "@/hooks/api/comment/usePostCommentMutation";
-import { useStoryQuery } from "@/hooks/api/story/useStoryQuery";
 import useModal from "@/hooks/useModal";
 
 import { getDefaultTextStyle } from "@/styles/getDefaultTextStyle";
 import { Theme } from "@/styles/Theme";
 
 import { convertToUTC } from "@/utils/convertToUTC";
+
+import type { StoryResultType } from "@/types/story";
 
 import {
 	layoutStyle,
@@ -27,14 +28,21 @@ import {
 	commentLayoutStyle,
 } from "@/components/Story/StoryDetailModal/StoryDetailModal.style";
 
-const StoryDetailModal = ({ storyId }: { storyId: number }) => {
-	const { storyData } = useStoryQuery(storyId);
-	const { commentData } = useCommentQuery(0, storyId);
+const StoryDetailModal = ({
+	boardId,
+	content,
+	hashtagList,
+	mediaList,
+	member,
+	createdDate,
+	recommendationInfo,
+}: StoryResultType) => {
+	const { commentData } = useCommentQuery(0, boardId);
 
 	const { mutate: postCommentMutation } = usePostCommentMutation();
 	const { mutate: editCommentMutation } = useEditCommentMutation();
 
-	const [content, setContent] = useState("");
+	const [commentContent, setCommentContent] = useState("");
 	const [mentionedMemberList] = useState<string[]>(["test"]);
 	const [commentButtonText, setCommentButtonText] = useState("등록");
 	const [commentId, setCommentId] = useState(0);
@@ -45,10 +53,10 @@ const StoryDetailModal = ({ storyId }: { storyId: number }) => {
 
 	const handleAddComment = () => {
 		postCommentMutation(
-			{ content, mentionedMemberList, boardId },
+			{ content: commentContent, mentionedMemberList, boardId },
 			{
 				onSuccess: () => {
-					setContent("");
+					setCommentContent("");
 				},
 			},
 		);
@@ -57,13 +65,13 @@ const StoryDetailModal = ({ storyId }: { storyId: number }) => {
 	const handleEditComment = () => {
 		editCommentMutation(
 			{
-				content,
+				content: commentContent,
 				mentionedMemberList,
 				commentId,
 			},
 			{
 				onSuccess: () => {
-					setContent("");
+					setCommentContent("");
 					setCommentId(0);
 				},
 			},
@@ -74,7 +82,7 @@ const StoryDetailModal = ({ storyId }: { storyId: number }) => {
 		if (!commentInputRef.current) return;
 
 		commentInputRef.current.focus();
-		setContent(content);
+		setCommentContent(content);
 		setCommentId(commentId);
 		setCommentButtonText("수정");
 	}, []);
@@ -82,135 +90,118 @@ const StoryDetailModal = ({ storyId }: { storyId: number }) => {
 	const handleDeleteStory = () => {
 		modal.openModal({
 			key: `DeleteWarningModal`,
-			component: () => <DeleteWarningModal targetId={storyId} target="story" />,
+			component: () => <DeleteWarningModal targetId={boardId} target="story" />,
 			isUpper: true,
 			notCloseIcon: true,
 		});
 	};
 
 	const handleEditStory = () => {
-		if (!storyData) return;
-
 		modal.closeModal();
 
 		modal.openModal({
 			key: `StoryUploadModal`,
 			component: () => (
 				<StoryUploadModal
-					mediaList={storyData.result.mediaList}
-					content={storyData.result.content}
-					hashtagList={storyData.result.hashtagList}
-					storyId={storyData.result.boardId}
+					mediaList={mediaList}
+					content={content}
+					hashtagList={hashtagList}
+					storyId={boardId}
 				/>
 			),
 		});
 	};
 
-	if (!storyData) {
-		return <div>로딩중...</div>;
-	}
-
 	if (!commentData) {
 		return <div>로딩중...</div>;
 	}
 
-	const boardId = storyData.result.boardId;
-	const ownerId = storyData.result.member.memberId;
-
 	return (
-		<>
-			{storyData && (
-				<Flex css={layoutStyle}>
-					{/* 미디어 영역 */}
-					<Flex css={sliderBoxStyle}>
-						<Carousel
-							width={740}
-							height={726}
-							borderRadius="36px 0 0 36px"
-							showArrows={storyData.result.mediaList.length > 1}
-							showDots={storyData.result.mediaList.length > 1}
-							length={storyData.result.mediaList.length}
-						>
-							{storyData.result.mediaList.map((imgUrl, index) => (
-								<Carousel.Item index={index} key={imgUrl}>
-									<img src={imgUrl} alt="img" />
-								</Carousel.Item>
-							))}
-						</Carousel>
-					</Flex>
+		<Flex css={layoutStyle}>
+			{/* 미디어 영역 */}
+			<Flex css={sliderBoxStyle}>
+				<Carousel
+					width={740}
+					height={726}
+					borderRadius="36px 0 0 36px"
+					showArrows={mediaList.length > 1}
+					showDots={mediaList.length > 1}
+					length={mediaList.length}
+				>
+					{mediaList.map((imgUrl, index) => (
+						<Carousel.Item index={index} key={imgUrl}>
+							<img src={imgUrl} alt="img" />
+						</Carousel.Item>
+					))}
+				</Carousel>
+			</Flex>
 
-					{/* 본문 영역 */}
-					<Flex styles={{ direction: "column" }}>
-						<Flex css={contentBoxStyle}>
-							{/* 프로필 영역 */}
-							<StoryProfile
-								img={storyData.result.member.profileImgUrl}
-								nickname={storyData.result.member.nickname}
-								editClick={handleEditStory}
-								deleteClick={handleDeleteStory}
-								ownerId={ownerId}
-							/>
+			{/* 본문 영역 */}
+			<Flex styles={{ direction: "column" }}>
+				<Flex css={contentBoxStyle}>
+					{/* 프로필 영역 */}
+					<StoryProfile
+						img={member.profileImgUrl}
+						nickname={member.nickname}
+						editClick={handleEditStory}
+						deleteClick={handleDeleteStory}
+						ownerId={member.memberId}
+					/>
 
-							{/* 콘텐츠 본문 영역 */}
-							<Box styles={{ maxWidth: "270px" }}>
-								<Text css={getDefaultTextStyle(Theme.color.input_text, 500)}>
-									{storyData.result.content}
-								</Text>
-							</Box>
+					{/* 콘텐츠 본문 영역 */}
+					<Box styles={{ maxWidth: "270px" }}>
+						<Text css={getDefaultTextStyle(Theme.color.input_text, 500)}>{content}</Text>
+					</Box>
 
-							{/* 게시 날짜 영역 */}
-							<Flex styles={{ justify: "flex-end", width: "100%" }}>
-								<Text size="xSmall" css={getDefaultTextStyle(Theme.color.readonly_text, 500)}>
-									{convertToUTC(new Date(storyData.result.createdDate)).date}
-								</Text>
-							</Flex>
-						</Flex>
-
-						{/* 구분선 */}
-						<Divider length="309px" />
-
-						{/* 코멘트 영역 */}
-						<Box css={commentLayoutStyle}>
-							{commentData.result.commentList.map((comment) => (
-								<Comment
-									key={comment.commentId}
-									commentId={comment.commentId}
-									content={comment.content}
-									createdDate={comment.createdDate}
-									member={comment.member}
-									handleEditClick={handleEditClick}
-								/>
-							))}
-						</Box>
-
-						<Divider length="309px" />
-
-						{/* 코멘트 작성 영역 */}
-						<Flex styles={{ direction: "column", gap: "10px", padding: "15px 24px" }}>
-							<Flex styles={{ align: "center", gap: "2px" }}>
-								<HeartEmptyIcon />
-
-								<Text size="small" css={getDefaultTextStyle(Theme.color.disabled_text, 600)}>
-									{storyData.result.recommendationInfo.recommendCount}
-								</Text>
-							</Flex>
-
-							<CommentInput
-								width="260px"
-								placeholder="댓글 작성"
-								handleButtonClick={
-									commentButtonText === "등록" ? handleAddComment : handleEditComment
-								}
-								content={content}
-								setContent={setContent}
-								commentInputRef={commentInputRef}
-								commentButtonText={commentButtonText}
-							/>
-						</Flex>
+					{/* 게시 날짜 영역 */}
+					<Flex styles={{ justify: "flex-end", width: "100%" }}>
+						<Text size="xSmall" css={getDefaultTextStyle(Theme.color.readonly_text, 500)}>
+							{convertToUTC(new Date(createdDate)).date}
+						</Text>
 					</Flex>
 				</Flex>
-			)}
-		</>
+
+				{/* 구분선 */}
+				<Divider length="309px" />
+
+				{/* 코멘트 영역 */}
+				<Box css={commentLayoutStyle}>
+					{commentData.result.commentList.map((comment) => (
+						<Comment
+							key={comment.commentId}
+							commentId={comment.commentId}
+							content={comment.content}
+							createdDate={comment.createdDate}
+							member={comment.member}
+							handleEditClick={handleEditClick}
+						/>
+					))}
+				</Box>
+
+				<Divider length="309px" />
+
+				{/* 코멘트 작성 영역 */}
+				<Flex styles={{ direction: "column", gap: "10px", padding: "15px 24px" }}>
+					<Flex styles={{ align: "center", gap: "2px" }}>
+						<HeartEmptyIcon />
+
+						<Text size="small" css={getDefaultTextStyle(Theme.color.disabled_text, 600)}>
+							{recommendationInfo.recommendCount}
+						</Text>
+					</Flex>
+
+					<CommentInput
+						width="260px"
+						placeholder="댓글 작성"
+						handleButtonClick={commentButtonText === "등록" ? handleAddComment : handleEditComment}
+						content={commentContent}
+						setContent={setCommentContent}
+						commentInputRef={commentInputRef}
+						commentButtonText={commentButtonText}
+					/>
+				</Flex>
+			</Flex>
+		</Flex>
 	);
 };
 
