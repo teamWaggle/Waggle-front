8b5cd5a0036d4d729c43b3eaa8c251d13ce2c636
sikkzz <sling0623@gmail.com>
2024-04-01 01:40:20 +0900

    issue #165 refactor: story profile refactoring

--- src/components/Story/StoryProfile/StoryProfile.tsx
+++ src/components/Story/StoryProfile/StoryProfile.tsx
@@ -1,17 +1,10 @@
-import { useState } from "react";
-
-import MoreButtonIcon from "@/assets/svg/ic-more-button.svg?react";
+import { css } from "@emotion/react";
 
 import { Flex, Text } from "@/components/common";
+import ProfileOptionMenu from "@/components/common/ProfileOptionMenu";
 
 import { getDefaultTextStyle } from "@/styles/getDefaultTextStyle";
 import { Theme } from "@/styles/Theme";
-
-import {
-	profileStyle,
-	moreButtonStyle,
-	menuStyle,
-} from "@/components/Story/StoryProfile/StoryProfile.style";
 
 interface ProfileType {
 	img: string | undefined;
@@ -22,35 +15,31 @@ interface ProfileType {
 }
 
 const StoryProfile = ({ img, nickname, ownerId, editClick, deleteClick }: ProfileType) => {
-	const [menuOpen, setMenuOpen] = useState(false);
-
 	const memberId = Number(localStorage.getItem("MEMBER_ID"));
 
 	return (
-		<Flex styles={{ align: "center", justify: "space-between", width: "100%" }}>
+		<Flex
+			styles={{ align: "center", justify: "space-between", width: "100%", position: "relative" }}
+		>
 			<Flex styles={{ align: "center", gap: "10px" }}>
 				<img src={img} alt="profileImg" css={profileStyle} />
 				<Text size="small" css={getDefaultTextStyle(Theme.color.text, 700)}>
 					{nickname}
 				</Text>
 			</Flex>
+
 			{memberId === ownerId && (
-				<Flex
-					styles={{ justify: "flex-end" }}
-					css={moreButtonStyle}
-					onClick={() => setMenuOpen((prev) => !prev)}
-				>
-					<MoreButtonIcon />
-					{menuOpen && (
-						<ul css={menuStyle}>
-							<li onClick={editClick}>수정하기</li>
-							<li onClick={deleteClick}>삭제하기</li>
-						</ul>
-					)}
-				</Flex>
+				<ProfileOptionMenu handleEditMenu={editClick} handleDeleteMenu={deleteClick} isLeft />
 			)}
 		</Flex>
 	);
 };
 
 export default StoryProfile;
+
+const profileStyle = css({
+	width: "33px",
+	height: "33px",
+	borderRadius: "50%",
+	objectFit: "cover",
+});
