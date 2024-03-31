648be3be04240d6ae53bd54a2de161177c9cce0a
sikkzz <sling0623@gmail.com>
2024-04-01 03:48:44 +0900

    issue #165 refactor: story detail modal props refactoring

--- src/components/Story/StoryCard/StoryCard.tsx
+++ src/components/Story/StoryCard/StoryCard.tsx
@@ -3,6 +3,7 @@ import MediaIcon from "@/assets/svg/ic-many-media.svg?react";
 import { Flex } from "@/components/common";
 import StoryDetailModal from "@/components/Story/StoryDetailModal/StoryDetailModal";
 
+import { useStoryQuery } from "@/hooks/api/story/useStoryQuery";
 import useModal from "@/hooks/useModal";
 
 import type { StoryListInfoType } from "@/types/story";
@@ -12,10 +13,22 @@ import { imgStyle, iconStyle } from "@/components/Story/StoryCard/StoryCard.styl
 const StoryCard = ({ boardId, thumbnail }: StoryListInfoType) => {
 	const modal = useModal();
 
+	const { storyData } = useStoryQuery(boardId);
+
 	const storyDetailOpen = () => {
 		modal.openModal({
 			key: `StoryDetail${boardId}`,
-			component: () => <StoryDetailModal storyId={boardId} />,
+			component: () => (
+				<StoryDetailModal
+					boardId={storyData.result.boardId}
+					content={storyData.result.content}
+					hashtagList={storyData.result.hashtagList}
+					mediaList={storyData.result.mediaList}
+					member={storyData.result.member}
+					createdDate={storyData.result.createdDate}
+					recommendationInfo={storyData.result.recommendationInfo}
+				/>
+			),
 		});
 	};
 
--- src/hooks/api/story/useStoryQuery.ts
+++ src/hooks/api/story/useStoryQuery.ts
@@ -1,13 +1,13 @@
 import type { AxiosError } from "axios";
 
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { getStory } from "@/api/story/getStory";
 
 import type { StoryType } from "@/types/story";
 
 export const useStoryQuery = (boardId: number) => {
-	const { data: storyData } = useQuery<StoryType, AxiosError>({
+	const { data: storyData } = useSuspenseQuery<StoryType, AxiosError>({
 		queryKey: ["story"],
 		queryFn: () => getStory(boardId),
 	});
