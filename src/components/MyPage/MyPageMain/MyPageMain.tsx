9cb20f4f8c3667e5035d51e09464f1c135b3c5ae
sikkzz <sling0623@gmail.com>
2024-04-02 23:42:57 +0900

    issue #173 design: my page profile styling

---
+++ src/components/MyPage/MyPageMain/MyPageMain.tsx
@@ -0,0 +1,11 @@
+import { layoutStyle, introductionBoxStyle } from "@/components/MyPage/MyPageMain/MyPageMain.style";
+
+const MyPageMain = () => {
+	return (
+		<div css={layoutStyle}>
+			<div css={introductionBoxStyle}></div>
+		</div>
+	);
+};
+
+export default MyPageMain;
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { css } from "@emotion/react";
+
+import { Flex } from "@/components/common";
+import MyPageMain from "@/components/MyPage/MyPageMain/MyPageMain";
+import MyPageProfile from "@/components/MyPage/MyPageProfile/MyPageProfile";
 
 const MyPage = () => {
-	return <div>MyPage</div>;
+	return (
+		<Flex css={layoutStyle}>
+			<MyPageProfile />
+			<MyPageMain />
+		</Flex>
+	);
 };
 
 export default MyPage;
+
+const layoutStyle = css({
+	maxWidth: "1536px",
+	margin: "0 auto",
+	padding: "0 196px",
+});
