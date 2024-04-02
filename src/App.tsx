0d211dfcaacf1f9d5f466b4a3bb629d0c558f80d
sikkzz <sling0623@gmail.com>
2024-04-02 22:49:30 +0900

    issue #173 refactor: logIn menu refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useLayoutEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
-import { useSetRecoilState } from "recoil";
-
+import LogIn from "@/components/common/LogIn/LogIn";
 import ScrollTop from "@/components/common/ScrollTop/ScrollTop";
 import Header from "@/components/Header/Header";
-
-import { isLoggedInState, memberIdState } from "@/recoil/atoms/auth";
 
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-	const setIsLoggedIn = useSetRecoilState(isLoggedInState);
-	const setMemberId = useSetRecoilState(memberIdState);
-
-	useLayoutEffect(() => {
-		if (localStorage.getItem("ACCESS_TOKEN")) {
-			setIsLoggedIn(true);
-		}
-
-		if (localStorage.getItem("MEMBER_ID")) {
-			setMemberId(Number(localStorage.getItem("MEMBER_ID")));
-		}
-	}, [setIsLoggedIn, setMemberId]);
-
 	return (
 		<>
 			<ScrollTop />
-			<Header />
-			<Outlet />
+			<LogIn>
+				<Header />
+				<Outlet />
+			</LogIn>
 			<ToastContainer position="top-center" style={{ width: "auto" }} autoClose={1000} />
 		</>
 	);
