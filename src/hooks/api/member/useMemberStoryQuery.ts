f3ea5fac51aaf53200f35e9aa007fab847013cc7
sikkzz <sling0623@gmail.com>
2024-04-04 18:49:29 +0900

    issue #182 feat: member story query add

---
+++ src/hooks/api/member/useMemberStoryQuery.ts
@@ -0,0 +1,25 @@
+import type { AxiosError } from "axios";
+
+import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
+
+import { getMemberStory } from "@/api/member/getMemberStory";
+
+import type { StoryListType } from "@/types/story";
+
+export const useMemberStoryQuery = (memberId: number) => {
+	const {
+		data: memberStoryData,
+		fetchNextPage,
+		hasNextPage,
+		isFetching,
+	} = useSuspenseInfiniteQuery<StoryListType, AxiosError>({
+		queryKey: ["memberStory"],
+		queryFn: ({ pageParam: currentPage }) => getMemberStory(memberId, currentPage),
+		initialPageParam: 0,
+		getNextPageParam: (lastPage) => {
+			return lastPage.result.nextPageParam === -1 ? undefined : lastPage.result.nextPageParam;
+		},
+	});
+
+	return { memberStoryData, fetchNextPage, hasNextPage, isFetching };
+};
