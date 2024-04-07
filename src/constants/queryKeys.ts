1f5d17aace04b11f85044d47e28fd1d96d8bbbdb
DongHyeonWon <dhe7700@naver.com>
2024-04-07 17:28:48 +0900

    issue #163 feat: add queryKeys

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -1,5 +1,12 @@
 export const QUERY_KEYS = {
+	//auth
+	REISSUE_TOKEN: "reissueToken",
+
 	SCHEDULE: "schedule",
 	SCHEDULE_MONTHLY: (year: number, month: number) => `schedule/${year}/${month}`,
+
 	MEMBER_TEAMS: "memberTeams",
+	TEAM_INFO: (teamId: number) => `teamInfo/${teamId}`,
+
+	TEAM_SCHEDULE_PAGE: (teamId: number) => `teamSchedule/${teamId}`,
 };
