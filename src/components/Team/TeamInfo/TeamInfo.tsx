9dabaeb314baf2630c1b83b67839467af73fe88d
DongHyeonWon <dhe7700@naver.com>
2024-04-08 12:16:14 +0900

    issue #192 feat: add TeamInfo api

--- src/components/Team/TeamInfo/TeamInfo.tsx
+++ src/components/Team/TeamInfo/TeamInfo.tsx
@@ -1,3 +1,5 @@
+import { useParams } from "react-router-dom";
+
 import GroupIcon from "@/assets/svg/group.svg?react";
 import LeftArrowIcon from "@/assets/svg/sm-left-arrow.svg?react";
 import RightArrowIcon from "@/assets/svg/sm-right-arrow.svg?react";
@@ -7,6 +9,8 @@ import MemberCard from "@/components/Team/TeamInfo/MemberSlider/MemberCard";
 import ParticipationCard from "@/components/Team/TeamInfo/ParticipationSlider/ParticipationCard";
 
 import { TEAM_INFO } from "@/constants/team";
+
+import { useTeamInfo } from "@/hooks/team/useTeamInfo";
 
 import {
 	leftArrowIconStyle,
@@ -19,52 +23,36 @@ import {
 	teamInfoSubTitleStyle,
 	teamSectionStyle,
 } from "@/components/Team/TeamInfo/TeamInfo.style";
-const members = [
-	"안녕초코야안녕",
-	"1",
-	"2",
-	"3",
-	"4",
-	"5",
-	"6",
-	"12",
-	"3",
-	"4",
-	"5",
-	"6",
-	"7",
-	"8",
-	"9",
-];
 
 const participatingMembers = ["안녕하ㄴㅇㄹㅇㅈ", "ade", "1", "2", "3", "4", "5", "6", "7", "8"];
 
 const TeamInfo = () => {
-	const memberLength = members.length;
+	const params = useParams();
+	console.log(params);
+	const teamId = Number(params.teamId);
+	const { name, description, teamMemberList, coverImageUrl, teamSize } = useTeamInfo(teamId) || {};
 	const participatingMemberLength = participatingMembers.length;
 	return (
 		<Flex css={teamSectionStyle} tag="section">
-			<img css={teamImgStyle} src="https://source.unsplash.com/random/300x300" />
+			<img css={teamImgStyle} src={coverImageUrl} />
 			<Box css={teamInfoBoxStyle}>
-				<Heading size="xLarge">신나는 강아지 유치원</Heading>
+				<Heading size="xLarge">{name}</Heading>
 				<Text size="xLarge" css={teamInfoSubTitleStyle}>
-					성북구에 있는 강아지 유치원이에요! 강아지, 사람과 함께 와글와글와글
+					{description}
 				</Text>
 				<Flex styles={{ align: "center" }}>
 					<GroupIcon />
 					<Text size="large" style={{ marginRight: "40px" }}>
-						{"3/7"}
+						{teamSize}/50
 					</Text>
 					<Slider
 						leftIcon={<LeftArrowIcon css={leftArrowIconStyle} />}
 						rightIcon={<RightArrowIcon css={rightArrowIconStyle} />}
 						cardBoxstyle={memberSliderBoxStyle}
 						displayCount={TEAM_INFO.MEMBERS_SLIDER_AMOUNT}
-						dataLength={memberLength}
+						dataLength={teamSize || 0}
 					>
-						{members.map((member, index) => (
-							<MemberCard key={index} member={member} />
-						))}
+						{teamMemberList?.map((member, index) => <MemberCard key={index} member={member} />)}
 					</Slider>
 				</Flex>
 				<Flex styles={{ align: "center", marginTop: "10px" }}>
