dea48b24956d1932c7226fde2c7df6adbb7491ca
sikkzz <sling0623@gmail.com>
2024-04-16 05:04:33 +0900

    issue #224 refactor: reply logic refactoring

--- packages/waggle-service/src/hooks/api/reply/useReplyQuery.ts
+++ packages/waggle-service/src/hooks/api/reply/useReplyQuery.ts
@@ -1,6 +1,6 @@
 import type { AxiosError } from "axios";
 
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { getReply } from "@/api/reply/getReply";
 
@@ -9,7 +9,7 @@ import { QUERY_KEYS } from "@/constants/queryKeys";
 import type { ReplyType } from "@/types/reply";
 
 export const useReplyQuery = (currentPage: number, commentId: number) => {
-  const { data: replyData } = useQuery<ReplyType, AxiosError>({
+  const { data: replyData } = useSuspenseQuery<ReplyType, AxiosError>({
     queryKey: [QUERY_KEYS.REPLY, commentId],
     queryFn: () => getReply(currentPage, commentId),
   });
