ac6045ac4249dd19c1500f23c34dbc442f7ae0d9
sikkzz <sling0623@gmail.com>
2024-04-29 01:06:57 +0900

    issue #257 design: connection card, searchbar styling

--- packages/waggle-service/src/components/Connection/ConnectionMain/ConnectionMain.tsx
+++ packages/waggle-service/src/components/Connection/ConnectionMain/ConnectionMain.tsx
@@ -2,11 +2,22 @@ import { css } from "@emotion/react";
 
 import { Flex, Box, Divider } from "waggle-design-system";
 
+import ConnectionSearchbar from "@/components/Connection/ConnectionMain/ConnectionSearchbar";
+import ConnectionCard from "@/components/Connection/ConnectionMain/ConnectionCard";
+
 const ConnectionMain = () => {
   return (
     <Box tag="main" css={mainBoxStyle}>
       <Flex styles={{ gap: "24px" }}>
-        <Flex styles={{ direction: "column", gap: "50px" }}></Flex>
+        <Flex styles={{ direction: "column", gap: "50px", marginTop: "32px" }}>
+          <ConnectionSearchbar />
+
+          <Box tag="ol" css={gridBoxStyle}>
+            <ConnectionCard />
+            <ConnectionCard />
+            <ConnectionCard />
+          </Box>
+        </Flex>
 
         <Divider direction="vertical" length="100vh" />
       </Flex>
@@ -20,4 +31,10 @@ const mainBoxStyle = css({
   maxWidth: "1536px",
   margin: "0 auto",
   padding: "0 196px",
+});
+
+const gridBoxStyle = css({
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gap: "16px",
 });
