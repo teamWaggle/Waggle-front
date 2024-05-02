8eea2f1776f998d31261c400ef845e51d72ebf1b
sikkzz <sling0623@gmail.com>
2024-05-03 00:03:35 +0900

    issue #265 feat: create siren category query

--- packages/waggle-service/src/constants/api.ts
+++ packages/waggle-service/src/constants/api.ts
@@ -35,6 +35,8 @@ export const END_POINTS = {
   SIREN_LIST: (curretPage: number) => `/api/sirens?currentPage=${curretPage}`,
   SIREN_FILTER: (filter: string, currentPage: unknown) =>
     `/api/sirens/filter?filterParam=${filter}&currentPage=${currentPage}`,
+  SIREN_CATEGORY: (category: string, currentPage: unknown) =>
+    `/api/sirens/category?category=${category}&currentPage=${currentPage}`,
   SIREN_REPRESENTATIVE: "/api/sirens/representative",
   POST_SIREN: "/api/sirens",
   REPLIES: (currentPage: number, commentId: number) =>
