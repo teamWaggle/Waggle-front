b00800f8cce21994db8d2af5760137547af44e80
sikkzz <sling0623@gmail.com>
2024-05-06 02:36:46 +0900

    issue #275 design: tooltip box styling

--- packages/waggle-service/src/components/Question/QuestionUpload/Keyword/Keyword.style.ts
+++ packages/waggle-service/src/components/Question/QuestionUpload/Keyword/Keyword.style.ts
@@ -2,6 +2,14 @@ import { css } from "@emotion/react";
 
 import { Theme } from "waggle-design-system";
 
+export const keywordBoxStyle = css({
+  display: "flex",
+  alignItems: "center",
+  gap: "8px",
+  marginTop: "20px",
+  position: "relative",
+});
+
 export const infoIconStyle = css({
   cursor: "pointer",
 });
@@ -12,4 +20,18 @@ export const keywordButtonBoxStyle = css({
   border: `1px solid ${Theme.color.border}`,
   borderRadius: "4px",
   boxShadow: "0px 0px 2px 0px rgba(0,0,0,0.25)",
+  cursor: "pointer",
+});
+
+export const tooltipBoxStyle = css({
+  position: "absolute",
+  top: "36px",
+  width: "321px",
+  height: "60px",
+  border: `1px solid ${Theme.color.border}`,
+  borderRadius: "10px",
+  boxShadow: "0px 2px 5px 0px rgba(0, 40, 37, 0.1)",
+  backgroundColor: Theme.color.white,
+  zIndex: 1,
+  padding: "10px 14px",
 });
--- packages/waggle-service/src/components/Question/QuestionUpload/Keyword/Keyword.tsx
+++ packages/waggle-service/src/components/Question/QuestionUpload/Keyword/Keyword.tsx
@@ -1,24 +1,44 @@
-import { Flex, Text, getDefaultTextStyle, Theme } from "waggle-design-system";
+import { useState, useRef } from "react";
+
+import { Flex, Box, Text, getDefaultTextStyle, Theme } from "waggle-design-system";
 
 import InformationIcon from "@/assets/svg/ic-information.svg?react";
 import PlusIcon from "@/assets/svg/ic-gallery-plus.svg?react";
 
+import useClickOutSide from "@/hooks/common/useClickOutSide";
+
 import {
+  keywordBoxStyle,
   infoIconStyle,
   keywordButtonBoxStyle,
+  tooltipBoxStyle,
 } from "@/components/Question/QuestionUpload/Keyword/Keyword.style";
 
 const Keyword = () => {
+  const [isToolTipOpen, setIsToolTipOpen] = useState(false);
+
+  const tooltipRef = useRef<HTMLDivElement>(null);
+
+  useClickOutSide(tooltipRef, () => setIsToolTipOpen(false));
+
   return (
-    <Flex styles={{ align: "center", gap: "8px", marginTop: "20px" }}>
-      <InformationIcon css={infoIconStyle} />
+    <div css={keywordBoxStyle} ref={tooltipRef}>
+      <InformationIcon css={infoIconStyle} onClick={() => setIsToolTipOpen((prev) => !prev)} />
       <Text size="xLarge" css={getDefaultTextStyle(Theme.color.readonly_text, 500)}>
         연관 키워드 추가
       </Text>
       <Flex styles={{ align: "center", justify: "center" }} css={keywordButtonBoxStyle}>
         <PlusIcon width={12} height={12} />
       </Flex>
-    </Flex>
+
+      {isToolTipOpen && (
+        <Box css={tooltipBoxStyle}>
+          <Text size="small" css={getDefaultTextStyle(Theme.color.readonly_text, 500)}>
+            상단에 노출되는 키워드를 통해 내 글을 소개해보세요! 공백 미포함 8글자, 특수문자 불가
+          </Text>
+        </Box>
+      )}
+    </div>
   );
 };
 
