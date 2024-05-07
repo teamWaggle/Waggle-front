0ac3b575c2f1ab9666ecc8ac958f5dfa16733593
sikkzz <sling0623@gmail.com>
2024-05-07 22:24:49 +0900

    issue #278 refactor: chat room type refactoring

--- packages/waggle-service/src/api/chat/getChatRoomList.ts
+++ packages/waggle-service/src/api/chat/getChatRoomList.ts
@@ -2,10 +2,10 @@ import { axiosInstance } from "@/api/axiosInstance";
 
 import { END_POINTS } from "@/constants/api";
 
-import type { ChatType } from "@/types/chat";
+import type { ChatListType } from "@/types/chat";
 
 export const getChatRoomList = async (currentPage: number) => {
-  const { data } = await axiosInstance.get<ChatType>(END_POINTS.CHAT_ROOM_LIST(currentPage));
+  const { data } = await axiosInstance.get<ChatListType>(END_POINTS.CHAT_ROOM_LIST(currentPage));
 
   return data;
 };
