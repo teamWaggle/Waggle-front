1081ff247691a658ea09f6519ff0ea112a990bbb
sikkzz <sling0623@gmail.com>
2024-05-07 22:18:55 +0900

    issue #278 feat: create chat room api

--- packages/waggle-service/src/constants/api.ts
+++ packages/waggle-service/src/constants/api.ts
@@ -71,6 +71,7 @@ export const END_POINTS = {
 
   CHAT_ROOM_LIST: (currentPage: number) => `/api/chat/rooms/paged?currentPage=${currentPage}`,
   CREATE_CHAT_ROOM: `/api/chat/rooms`,
+  CHAT_ROOM: (chatRoomId: number) => `/api/chat/rooms/${chatRoomId}`,
   // Schuedule
   MEMBER_SCHEDULES: (memberId: number) => `/api/schedules/members/${memberId}`,
   MEMBER_SCHEDULES_MONTHLY: (userUrl: string, year: number, month: number) =>
