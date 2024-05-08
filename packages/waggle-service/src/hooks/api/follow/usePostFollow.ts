77df0db12902aaeef245a233958bd1830f8e374d
sikkzz <sling0623@gmail.com>
2024-05-09 07:22:00 +0900

    issue #266 feat: create isFollow api

--- packages/waggle-service/src/hooks/api/follow/usePostFollow.ts
+++ packages/waggle-service/src/hooks/api/follow/usePostFollow.ts
@@ -1,10 +1,22 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { postFollow } from "@/api/follow/postFollow";
 
+import { QUERY_KEYS } from "@/constants/queryKeys";
+
 export const usePostFollow = () => {
+  const queryClient = useQueryClient();
+
   const followMutation = useMutation({
     mutationFn: postFollow,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.FOLLOW],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.MEMBER_INFO],
+      });
+    },
   });
 
   return followMutation;
