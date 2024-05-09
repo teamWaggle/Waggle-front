709b333bde2ce3d3aaeb32d9aee08a5b2570f09d
sikkzz <sling0623@gmail.com>
2024-05-09 09:20:02 +0900

    issue #281 refactor: siren bio skeleton add

--- packages/waggle-service/src/components/Story/StoryMain/StoryMainSkeleton.tsx
+++ packages/waggle-service/src/components/Story/StoryMain/StoryMainSkeleton.tsx
@@ -1,20 +1,19 @@
-import { Flex, Box } from "waggle-design-system";
+import { Flex, Box, Skeleton } from "waggle-design-system";
 
 import Sidebar from "@/components/Sidebar/Sidebar";
-import StoryCardSkeleton from "@/components/Story/StoryCard/StoryCardSkeleton";
 import StorySearchBar from "@/components/Story/StorySearchBar/StorySearchBar";
 
 import { gridBoxStyle } from "@/components/Story/StoryMain/StoryMain.style";
 
 const StoryMainSkeleton = () => {
   return (
-    <Box tag="section" styles={{ padding: "32px 0 60px" }}>
+    <Box styles={{ padding: "32px 0 60px" }}>
       <Flex styles={{ gap: "30px", justify: "center" }}>
         <Box>
           <StorySearchBar />
           <Box tag="ol" css={gridBoxStyle}>
             {Array.from({ length: 9 }, (_, index) => (
-              <StoryCardSkeleton key={index} />
+              <Skeleton width="252px" height="252px" key={index} />
             ))}
           </Box>
         </Box>
