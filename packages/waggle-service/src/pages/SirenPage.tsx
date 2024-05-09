eca7640adcfe70ae6f12c769e64f8b6e7963d273
sikkzz <sling0623@gmail.com>
2024-05-09 09:08:33 +0900

    issue #281 refactor: siren skeletion add

--- packages/waggle-service/src/pages/SirenPage.tsx
+++ packages/waggle-service/src/pages/SirenPage.tsx
@@ -1,15 +1,21 @@
 import { css } from "@emotion/react";
+import { Suspense } from "react";
 
 import { Box } from "waggle-design-system";
 
 import SirenBio from "@/components/Siren/SirenBio/SirenBio";
 import SirenMain from "@/components/Siren/SirenMain/SirenMain";
+import SirenMainSkeleton from "@/components/Siren/SirenMain/SirenMainSkeleton";
 
 const SirenPage = () => {
   return (
     <Box css={boxStyle}>
-      <SirenBio />
-      <SirenMain />
+      <Suspense fallback={<div />}>
+        <SirenBio />
+      </Suspense>
+      <Suspense fallback={<SirenMainSkeleton />}>
+        <SirenMain />
+      </Suspense>
     </Box>
   );
 };
