858537a1d9889412ad56af4d309c654d6dc8c1f1
sikkzz <sling0623@gmail.com>
2024-05-10 17:47:10 +0900

    issue #278 feat: create chat room memberlist

--- packages/waggle-service/src/components/Connection/Chat/ChatRoomInfoBox/ChatRoomInfoBox.tsx
+++ packages/waggle-service/src/components/Connection/Chat/ChatRoomInfoBox/ChatRoomInfoBox.tsx
@@ -6,26 +6,48 @@ import { Flex, Box, Heading, Text, Theme, getDefaultTextStyle } from "waggle-des
 import PersonIcon from "@/assets/svg/ic-connection-person.svg?react";
 
 import RoomSettingButton from "@/components/Connection/Chat/ChatRoomInfoBox/RoomSettingButton";
+import MemberList from "@/components/common/MemberList/MemberList";
 
 import { ChatRoomContext } from "@/components/Connection/Chat/ChatRoomModal/ChatRoomModal";
+
+import { useMemberListTrigger } from "@/hooks/common/useMemberListTrigger";
 
 const ChatRoomInfoBox = () => {
   const context = useContext(ChatRoomContext);
 
   if (!context) throw Error("context error");
 
-  const { name, description, memberCount } = context;
+  const { name, description, memberCount, memberList } = context;
+
+  const { isMemberListOpen, handleMemberList, handleMemberListClose, memberListRef } =
+    useMemberListTrigger();
+
+  console.log(memberList);
 
   return (
     <Box css={titleBoxStyle}>
       <Flex styles={{ gap: "16px", align: "center" }}>
         <Heading css={getDefaultTextStyle(Theme.color.white, 700)}>{name}</Heading>
-        <Flex styles={{ align: "center", gap: "4px" }} css={personBoxStyle}>
+        <Flex
+          styles={{ align: "center", gap: "4px" }}
+          css={personBoxStyle}
+          ref={memberListRef}
+          onClick={handleMemberList}
+        >
           <PersonIcon />
           <Text size="small" css={getDefaultTextStyle(Theme.color.white, 600)}>
             {memberCount}/7
           </Text>
+
+          {isMemberListOpen && (
+            <MemberList
+              title="채팅방 멤버"
+              listData={memberList}
+              handleClose={handleMemberListClose}
+            />
+          )}
         </Flex>
+
         <RoomSettingButton />
       </Flex>
       <Text css={getDefaultTextStyle(Theme.color.white, 500)}>{description}</Text>
