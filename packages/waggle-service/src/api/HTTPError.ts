fb188421e2d3384e8f45cbfd50330fdac6e05804
DongHyeonWon <dhe7700@naver.com>
2024-05-13 20:08:11 +0900

    issue #276 feat: add detail in HTTPError

--- packages/waggle-service/src/api/HTTPError.ts
+++ packages/waggle-service/src/api/HTTPError.ts
@@ -1,14 +1,15 @@
 export class HTTPError extends Error {
   statusCode: number;
-
+  detail?: string;
   code?: number;
 
-  constructor(statusCode: number, message?: string, code?: number) {
+  constructor(statusCode: number, message?: string, code?: number, detail?: string) {
     super(message);
 
     this.name = "HTTPError";
     this.statusCode = statusCode;
     this.code = code;
+    this.detail = detail;
 
     Object.setPrototypeOf(this, HTTPError.prototype);
   }
