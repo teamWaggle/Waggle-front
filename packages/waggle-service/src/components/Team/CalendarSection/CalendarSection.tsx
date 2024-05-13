e7265a9858d81c68fc3eb26b343a0a6223b2a7f5
DongHyeonWon <dhe7700@naver.com>
2024-05-13 15:35:17 +0900

    issue #276 feat: add calendar teamcolor

--- packages/waggle-service/src/components/Team/CalendarSection/CalendarSection.tsx
+++ packages/waggle-service/src/components/Team/CalendarSection/CalendarSection.tsx
@@ -4,15 +4,19 @@ import Calendar from "@/components/Planning/Calendar/Calendar";
 import { sectionStyle } from "@/components/Team/CalendarSection/CalendarSection.style";
 import useCalendar from "@/hooks/common/useCalendar";
 import { useCalendarSchedule } from "@/hooks/common/useCalendarSchedules";
+import { useTeamInfo } from "@/hooks/api/team/useTeamInfo";
+import { useParamsTeamId } from "@/hooks/team/useParamsTeamId";
 
 const CalendarSection = () => {
   const { currentDate, handlePrevDate, handleNextDate } = useCalendar();
   const scheduleList = useCalendarSchedule(currentDate);
+  const teamId = useParamsTeamId();
+  const { teamColor } = useTeamInfo(teamId);
   return (
     <Flex
       tag="section"
       styles={{ position: "relative", direction: "column", align: "center" }}
-      css={sectionStyle("team_1")}
+      css={sectionStyle(teamColor)}
     >
       <Calendar
         scheduleList={scheduleList}
