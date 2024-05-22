a40e354b5d26e8de180afb7b8ba83aaee237a1a1
sikkzz <sling0623@gmail.com>
2024-05-23 04:02:54 +0900

    issue #289 feat: chat message history add

--- packages/waggle-service/src/components/Connection/Chat/ChatRoomContent/ChatMessage/ChatMessage.tsx
+++ packages/waggle-service/src/components/Connection/Chat/ChatRoomContent/ChatMessage/ChatMessage.tsx
@@ -2,22 +2,27 @@ import { css } from "@emotion/react";
 
 import { Flex, Box, Text, Theme, getDefaultTextStyle } from "waggle-design-system";
 
-import SampleImg from "@/assets/png/post-sample.png";
+import { convertToUTC } from "@/utils/convertToUTC";
 
-const ChatMessage = () => {
+import type { ChatMessageInfoType } from "@/types/chat";
+
+const ChatMessage = ({ chatMessageInfo }: ChatMessageInfoType) => {
   return (
-    <Flex styles={{ gap: "24px" }}>
-      <img src={SampleImg} alt="profileImg" css={profileImgStyle} />
-      <Box>
+    <Flex styles={{ gap: "24px", width: "100%" }}>
+      <img src={chatMessageInfo.sender.profileImgUrl} alt="profileImg" css={profileImgStyle} />
+      <Flex styles={{ direction: "column" }}>
         <Flex styles={{ gap: "16px", align: "center" }}>
-          <Text css={getDefaultTextStyle(Theme.color.text, 500)}>활발한 강아지</Text>
-          <Text css={getDefaultTextStyle(Theme.color.readonly_text, 300)}>오후 4:30</Text>
+          <Text css={getDefaultTextStyle(Theme.color.text, 500)}>
+            {chatMessageInfo.sender.nickname}
+          </Text>
+          <Text css={getDefaultTextStyle(Theme.color.readonly_text, 300)}>
+            {convertToUTC(new Date(chatMessageInfo.sendTime)).date}
+          </Text>
         </Flex>
         <Box css={messageBoxStyle}>
-          네..으으응믕므..고민네..으으응믕므. .고민네..으으응믕므..고민네.
-          .으으응믕므..고민네..으으응믕므..고민
+          <Text>{chatMessageInfo.content}</Text>
         </Box>
-      </Box>
+      </Flex>
     </Flex>
   );
 };
