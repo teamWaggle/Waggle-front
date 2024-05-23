d69907869f306ad8c6674e05f21db3ad2316da5d
DongHyeonWon <dhe7700@naver.com>
2024-05-23 17:05:37 +0900

    issue #276 fix: preview error

--- packages/waggle-service/src/hooks/common/useImagePreview.ts
+++ packages/waggle-service/src/hooks/common/useImagePreview.ts
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 
-const useImagePreview = (value: File | null) => {
+const useImagePreview = (value: File | string | null) => {
   const [imagePreview, setImagePreview] = useState<string | null>(null);
 
   useEffect(() => {
-    const newUrl = value ? URL.createObjectURL(value) : null;
+    const newUrl = value instanceof File ? URL.createObjectURL(value) : value;
     if (newUrl !== imagePreview) {
       URL.revokeObjectURL(imagePreview || "");
       setImagePreview(newUrl);
