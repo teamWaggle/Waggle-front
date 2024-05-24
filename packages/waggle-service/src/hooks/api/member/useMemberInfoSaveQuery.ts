bf3925481badde71fc0c0f018bcb176cd2c6d11c
DongHyeonWon <dhe7700@naver.com>
2024-05-24 16:55:35 +0900

    issue #276 feat: add NonMemberPublicOnly

--- packages/waggle-service/src/hooks/api/member/useMemberInfoSaveQuery.ts
+++ packages/waggle-service/src/hooks/api/member/useMemberInfoSaveQuery.ts
@@ -1,20 +1,24 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { isLoggedInState } from "@/recoil/atoms/auth";
+import { useQuery } from "@tanstack/react-query";
 
 import { getMemberInfoSave } from "@/api/member/getMemberInfoSave";
 
 import { QUERY_KEYS } from "@/constants/queryKeys";
+import { useRecoilValue } from "recoil";
 
 export const useMemberInfoSaveQuery = () => {
-  const { data } = useSuspenseQuery({
+  const isLoggedIn = useRecoilValue(isLoggedInState);
+  const { data } = useQuery({
     queryKey: [QUERY_KEYS.MEMBER_INFO_SAVE],
-    queryFn: getMemberInfoSave,
+    queryFn: getMemberInfoSave, // Update the queryFn assignment
+    enabled: isLoggedIn === true,
     staleTime: Infinity,
     retry: false,
     meta: { errorMessage: false },
   });
 
-  const memberId = data.result.memberId;
-  const userUrl = data.result.userUrl;
+  const memberId = data?.result.memberId;
+  const userUrl = data?.result.userUrl;
 
   return { memberId, userUrl };
 };
