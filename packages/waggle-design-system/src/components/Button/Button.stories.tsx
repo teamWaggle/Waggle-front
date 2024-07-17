049cd4eacab342cc3c0e9643c578ebe19318a454
sikkzz <sling0623@gmail.com>
2024-07-17 16:24:44 +0900

    issue #309 refactor: button story refactoring

--- packages/waggle-design-system/src/components/Button/Button.stories.tsx
+++ packages/waggle-design-system/src/components/Button/Button.stories.tsx
@@ -27,82 +27,19 @@ const meta: Meta<typeof Button> = {
   },
 };
 
-export const Variants: StoryObj<typeof meta> = {
-  render: ({ size, children }) => (
+export const Primary: StoryObj<typeof meta> = {
+  render: ({ variant, size, children }) => (
     <ul css={containerStyle}>
       <li css={informationStyle}>
-        <h6>Default</h6>
-        <Button variant="default" size={size}>
-          {children}
-        </Button>
-      </li>
-      <li css={informationStyle}>
-        <h6>Disabled</h6>
-        <Button variant="disabled" size={size}>
-          {children}
-        </Button>
-      </li>
-      <li css={informationStyle}>
-        <h6>white</h6>
-        <Button variant="white" size={size}>
-          {children}
-        </Button>
-      </li>
-      <li css={informationStyle}>
-        <h6>outline</h6>
-        <Button variant="outline" size={size}>
-          {children}
-        </Button>
-      </li>
-      <li css={informationStyle}>
-        <h6>danger</h6>
-        <Button variant="danger" size={size}>
+        <h6>
+          <span>size : {size}</span>variants : {variant}
+        </h6>
+        <Button variant={variant} size={size}>
           {children}
         </Button>
       </li>
     </ul>
   ),
-  argTypes: {
-    variant: {
-      control: false,
-    },
-  },
-};
-
-export const Sizes: StoryObj<typeof meta> = {
-  render: ({ variant, children }) => (
-    <ul css={containerStyle}>
-      <li css={informationStyle}>
-        <h6>Small</h6>
-        <Button variant={variant} size="small">
-          {children}
-        </Button>
-      </li>
-      <li css={informationStyle}>
-        <h6>Medium</h6>
-        <Button variant={variant} size="medium">
-          {children}
-        </Button>
-      </li>
-      <li css={informationStyle}>
-        <h6>Large</h6>
-        <Button variant={variant} size="large">
-          {children}
-        </Button>
-      </li>
-      <li css={informationStyle}>
-        <h6>xLarge</h6>
-        <Button variant={variant} size="xLarge">
-          {children}
-        </Button>
-      </li>
-    </ul>
-  ),
-  argTypes: {
-    size: {
-      control: false,
-    },
-  },
 };
 
 export default meta;
